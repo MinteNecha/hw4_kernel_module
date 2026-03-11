2871f70b26eed8195cda70484e25d64833da532f
Minte <mintenecha@gmail.com>
2026-03-11 20:23:51 +0000

    Another push, to alter run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-
+echo "----------------------------Making and input module-------------------------"
+sleep 1
 make
 sudo insmod hw4.ko
 sudo dmesg | tail -2
 
-sleep 1
+sleep 2
 
+echo "------------------Creating node to be able to read and write to device------------------------------------"
 MAJOR=$(dmesg | grep "Welcome Major number: " | tail -n 1 | awk '{print $NF}')
 sudo mknod /dev/hw4 c $MAJOR 0
 sudo chmod 666 /dev/hw4
 
-sleep 1
+sleep 2
 
-echo "Writting to device and reading from device" > /dev/hw4
+echo "----------------Lets write to device-----------------------------"
+echo "Message to device" > /dev/hw4
 
-sleep 1
+sleep 2
 
 head -n 1 /dev/hw4
 
-sleep 1
+sleep 2
 
+echo "-------------------Time to remove everything----------------------"
 sudo rm /dev/hw4
 sudo rmmod hw4
 make clean
